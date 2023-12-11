e01e4a4a435bd3708633e1b8e402cd3c4b5f86f7
SusieDavtian <susiedavtian@gmail.com>
2023-12-11 14:44:26 +0000

    Add logging

--- mycliscript.sh
+++ mycliscript.sh
@@ -9,27 +9,41 @@ ROUTE_TABLE_ID="rtb-01c6e91273b756584"
 KEY_PAIR_NAME="mynewapp"
 
 # 1. Create VPC
-vpc_id=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --region "$AWS_REGION" --query 'Vpc.VpcId' --output json)
+echo "Creating VPC"
+vpc_id=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --region "$AWS_REGION" --query 'Vpc.VpcId' --output text)
+echo "VPC is created. VPC ID: $vpc_id"
 
 # 2. Create Internet Gateway
-internet_gateway_id=$(aws ec2 create-internet-gateway --region "$AWS_REGION" --query 'InternetGateway.InternetGatewayId' --output json)
+echo "Creating Internet Gateway"
+internet_gateway_id=$(aws ec2 create-internet-gateway --region "$AWS_REGION" --query 'InternetGateway.InternetGatewayId' --output text)
+echo "Internet Gateway Created. IG ID: $internet_gateway_id"
 
 # 3. Attach Internet Gateway to VPC
-aws ec2 attach-internet-gateway --internet-gateway-id "$internet_gateway_id" --vpc-id "$vpc_id" --region "$AWS_REGION" --output json
+echo "Attaching Internet Gateway to VPC"
+aws ec2 attach-internet-gateway --internet-gateway-id "$internet_gateway_id" --vpc-id "$vpc_id" --region "$AWS_REGION" --output text
+echo "IG is Attached to VPC"
 
 # 4. Create Subnet
-subnet_id=$(aws ec2 create-subnet --vpc-id "$vpc_id" --cidr-block 10.0.0.0/24 --region "$AWS_REGION" --query 'Subnet.SubnetId' --output json)
+echo "Creating a Subnet"
+subnet_id=$(aws ec2 create-subnet --vpc-id "$vpc_id" --cidr-block 10.0.0.0/24 --region "$AWS_REGION" --query 'Subnet.SubnetId' --output text)
+echo "Subnet is created. Subnet ID: $subnet_id"
 
 # 5. Create Route Table
-route_table_id=$(aws ec2 create-route-table --vpc-id "$vpc_id" --region "$AWS_REGION" --query 'RouteTable.RouteTableId' --output json)
+echo "Creating a Routing Table"
+route_table_id=$(aws ec2 create-route-table --vpc-id "$vpc_id" --region "$AWS_REGION" --query 'RouteTable.RouteTableId' --output text)
+echo "Route Table is created. RT ID: $route_table_id"
+
 
 # 6. Create Route for Internet Gateway in Route Table
-aws ec2 create-route --route-table-id "$route_table_id" --destination-cidr-block 0.0.0.0/0 --gateway-id "$internet_gateway_id" --region "$AWS_REGION" --output json
+echo "Creating route for Internet Gateway"
+aws ec2 create-route --route-table-id "$route_table_id" --destination-cidr-block 0.0.0.0/0 --gateway-id "$internet_gateway_id" --region "$AWS_REGION" --output text
 
 # 7. Associate Subnet with Route Table
-aws ec2 associate-route-table --subnet-id "$subnet_id" --route-table-id "$route_table_id" --region "$AWS_REGION" --output json
+echo "Associating subnet with route table"
+aws ec2 associate-route-table --subnet-id "$subnet_id" --route-table-id "$route_table_id" --region "$AWS_REGION" --output text
 
 # Launch an EC2 instance
+echo "Launcing an EC2 instance"
 aws ec2 run-instances \
   --image-id ami-0e35da80743b7a307 \
   --instance-type t2.micro \
@@ -37,5 +51,5 @@ aws ec2 run-instances \
   --subnet-id "$subnet_id" \
   --region "$AWS_REGION" \
 
-echo "My script is working! EC2 instance launched."
+echo "My script is working! EC2 instance has been launched succesfully."
 
